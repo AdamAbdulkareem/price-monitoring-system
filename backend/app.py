840c260043f60c9e27bd96b5c38796a20407ce56
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-09-30 19:48:12 +0100

    feat: Add a new route scrape_amazon_product

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import asyncio
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from .scraper import main
+from .scraper import select_product
 from .fetchDatabase import fetch_dbProduct
 
 
@@ -21,7 +22,14 @@ def scrape_amazon():
     product_data = asyncio.run(main(amazon_website, product_name))
     return jsonify({"products": product_data})
 
-
+@app.route("/scrape_amazon_product", methods=["POST"])
+def scrape_amazon_product():
+    # Call the scrape product function from scraper.py
+    data = request.get_json()
+    data_asin = data.get("data_asin")
+    product_page_url = f"https://www.amazon.com/dp/{data_asin}"
+    asyncio.run(select_product(product_page_url))
+    return jsonify("The request was successful")
     
 @app.route("/fetch_product_history", methods=["GET"])
 def fetch_product_history():
